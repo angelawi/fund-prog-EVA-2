b8a564aa8e5dd9bdff43b57d332c72d97354e37a
angelawi <143562997+angelawi@users.noreply.github.com>
2023-10-30 17:47:53 -0600

    Add files via upload

---
+++ EVA2_20_WHILE_RETIRO/src/eva2_20_while_retiro/input.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_20_while_retiro;
+
+/**
+ *
+ * @author invitado
+ */
+class input {
+
+    static int nextInt() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
