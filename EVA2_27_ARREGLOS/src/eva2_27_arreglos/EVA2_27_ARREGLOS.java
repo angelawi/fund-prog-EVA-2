6653b09e613a31c8188c775c1360c5b3062fa9ae
angelawi <143562997+angelawi@users.noreply.github.com>
2023-11-08 16:50:13 -0700

    Add files via upload

---
+++ EVA2_27_ARREGLOS/src/eva2_27_arreglos/EVA2_27_ARREGLOS.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_27_arreglos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_27_ARREGLOS {
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+      // TODO code application logic here
+//declaramos un arreglo que almacena 10 neteros.
+      //acceso a los arreglos es aleatorio.
+      //se accede a los valores a travez de un indice
+      //los valores almacenados tienen una direccion:
+      //el primer elemento esta en la posicion 0
+      //el ultimo en la posicion N - 1, donde N es el tamaño 
+      //del arreglos
+      int [] arregloEnteros = new int[10];
+      //ACCEDER VALORES EN UN ARREGLO:
+      arregloEnteros[0]=100;
+      arregloEnteros[1]=100;
+      arregloEnteros[3]=100;
+      arregloEnteros[4]=100;
+      arregloEnteros[5]=100;
+      arregloEnteros[6]=100;
+      arregloEnteros[7]=100;
+      arregloEnteros[8]=100;
+      arregloEnteros[9]=100;
+      arregloEnteros[10]=100;
+System.out.println("valor en posicion 0: "+ arregloEnteros[0]);
+String[] arreglocade;
+      arreglocade = new String[5];
+arreglocade[0]= "hola";
+arreglocade[1]="";
+arreglocade[2]="hola";
+arreglocade[3]="";
+arreglocade[4]="cruel!!";
+
+int []arreglo={5,4,3,2,1};
+      System.out.println("posicion arreglo [3:"+ arreglo[3]);
+       
+
+
+      
+      
+   }
+   
+}
