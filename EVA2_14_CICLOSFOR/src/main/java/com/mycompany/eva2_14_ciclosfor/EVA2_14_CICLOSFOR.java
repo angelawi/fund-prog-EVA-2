3a6b8493c81ceb702da201e349912f30f7f9df22
angelawi <143562997+angelawi@users.noreply.github.com>
2023-11-15 18:12:57 -0700

    Add files via upload

---
+++ EVA2_14_CICLOSFOR/src/main/java/com/mycompany/eva2_14_ciclosfor/EVA2_14_CICLOSFOR.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_14_ciclosfor;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA2_14_CICLOSFOR {
+
+    public static void main(String[] args) {
+         for (int i = 0; i < 10; i++) {
+            System.out.println(i);    
+        }
+        
+        System.out.print("---------------");
+        for(int i=9; i >=0; i--){
+            System.out.println(i);
+        }
+        for(int i=0; i<=100;i+=2)
+            System.out.print(i+"-");
+    }
+}
+    
