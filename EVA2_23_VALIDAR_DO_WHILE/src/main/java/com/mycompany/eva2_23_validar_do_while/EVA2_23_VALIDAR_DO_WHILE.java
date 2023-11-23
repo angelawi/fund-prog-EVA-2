46847cbcfb1ad79950722c842e49cffcd34ea515
angelawi <143562997+angelawi@users.noreply.github.com>
2023-11-22 21:00:56 -0700

    Add files via upload

---
+++ EVA2_23_VALIDAR_DO_WHILE/src/main/java/com/mycompany/eva2_23_validar_do_while/EVA2_23_VALIDAR_DO_WHILE.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_23_validar_do_while;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA2_23_VALIDAR_DO_WHILE {
+
+    public static void main(String[] args) {
+         Scanner input=new Scanner(System.in);
+        int valor;
+        String cade;
+        
+        do{
+            System.out.println("introduce un valor enter: ");
+            valor=input.nextInt();
+        }while(true);
+    }
+}
