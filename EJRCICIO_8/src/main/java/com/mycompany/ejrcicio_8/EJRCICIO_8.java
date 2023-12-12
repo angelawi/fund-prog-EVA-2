714c5ac497b3cb8f2acdfb3764465d0c4e9035d3
angelawi <143562997+angelawi@users.noreply.github.com>
2023-12-12 12:14:14 -0600

    Add files via upload

---
+++ EJRCICIO_8/src/main/java/com/mycompany/ejrcicio_8/EJRCICIO_8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejrcicio_8;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EJRCICIO_8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
