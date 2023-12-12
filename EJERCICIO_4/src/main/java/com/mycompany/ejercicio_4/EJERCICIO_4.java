453d1e3aae2a5f8e9d849f1a598ce8f225a46037
angelawi <143562997+angelawi@users.noreply.github.com>
2023-12-12 12:07:26 -0600

    Add files via upload

---
+++ EJERCICIO_4/src/main/java/com/mycompany/ejercicio_4/EJERCICIO_4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_4;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EJERCICIO_4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
